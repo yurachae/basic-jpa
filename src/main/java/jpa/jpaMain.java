4af9f57ef8e57f16a8fe7cb4437d6add90e4c708
YURACHAE\yurachae <yura920501@gmail.com>
2022-06-17 14:44:37 +0900

    JPQL - 객체 대상 쿼리문

--- src/main/java/jpa/jpaMain.java
+++ src/main/java/jpa/jpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class jpaMain {
     public static void main(String[] args) {
@@ -25,15 +26,25 @@ public class jpaMain {
 //            em.persist(member);//jpa 실행
 
             //회원조회
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("name = " + findMember.getName());
-            System.out.println("id = " + findMember.getId());
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("name = " + findMember.getName());
+//            System.out.println("id = " + findMember.getId());
 
             //회원삭제
 //            em.remove(findMember);
 
             //회원수정
-            findMember.setName("memberB");
+//            findMember.setName("memberB");
+
+            //JPQL - 객체를 대상으로 쿼리!
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(1)  //페이징
+                    .setMaxResults(10)  //페이징
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member = " + member.getId());
+            }
+
 
             tx.commit(); //transaction 정상적으로 끝날 때 commit
         }catch (Exception e){
